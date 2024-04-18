20d22b19055c34fbafbf4407cfcb5d658d346bb6
GuilhermeMoreira-DEV <116835175+GuilhermeMoreira-DEV@users.noreply.github.com>
2024-04-17 22:27:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,14 +2,11 @@ var degree = 1800;
 var clicks = 0;
 
 $(document).ready(function(){
-
     $('#spin').click(function(){
         clicks++;
 
         var newDegree = degree * clicks;
-        // Remove a geração aleatória de graus extras
-        // var extraDegree = Math.floor(Math.random()* (360 - 1 + 1)) +1;
-        totalDegree = newDegree; // Apenas usa o novo grau, sem o aleatório
+        totalDegree = newDegree;
 
         $('#roleta .sec').each(function(){
             var t = $(this);
@@ -24,39 +21,31 @@ $(document).ready(function(){
                 }
                 var aoY = to.offset().top;
                 $('#txt').html(aoY);
-                console.log(aoY);
 
                 if(aoY < 23.83){
-                    console.log('<<<');
                     $('#spin').addClass('spin');
                     setTimeout(function(){
                         $('#spin').removeClass('spin');
 
-                    },100);
+                    }, 100);
                 }
-            },10);
+            }, 10);
 
             $('#interno-roleta').css({
-                'transform': 'rotate(' + totalDegree + 'deg)'
+                'transform': 'rotate(' + totalDegree + 'deg)',
+                'transition-duration': '8s' // Ajuste o tempo de duração da animação para 8 segundos
             });
             noY = t.offset().top;
         });
+
+        // Exibir pop-up após 8 segundos
+        setTimeout(function() {
+            $('#popup').fadeIn();
+        }, 8000); // 8000 milissegundos = 8 segundos
     });
-    $(document).ready(function(){
-        $('#spin').click(function(){
-            // Seu código existente para girar a roleta aqui...
-    
-            // Simulando o término do giro após algum tempo (você pode substituir isso pelo seu código real)
-            setTimeout(function() {
-                // Exibir pop-up após 2 segundos
-                $('#popup').fadeIn();
-            }, 7000); // 2000 milissegundos = 2 segundos
-        });
-    
-        // Fechar o pop-up quando clicar no botão de fechar
-        $('#close').click(function(){
-            $('#popup').fadeOut();
-        });
+
+    // Fechar o pop-up quando clicar no botão de fechar
+    $('#close').click(function(){
+        $('#popup').fadeOut();
     });
-    
 });
